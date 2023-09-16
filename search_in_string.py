0a18637d010772442f79a3433e3ba8cdf62eaf20
Korinisarga <137990367+Korinisarga@users.noreply.github.com>
2023-09-16 17:25:32 +0530

    Add files via upload

---
+++ search_in_string.py
@@ -0,0 +1,12 @@
+
+def  search(nam,target1):
+    if len(nam)==0:
+        return False
+    i=0
+    while i<len(nam):
+     i+i
+     if target1==nam[i]:
+        return True
+name="hello"
+target='a','e','i','o','u'
+print(search(name,target))
